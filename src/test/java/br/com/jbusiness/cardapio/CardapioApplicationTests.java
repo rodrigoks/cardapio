7a1d7a01c7b8586e961aa3e7eeab344a1dad55f5
Rodrigo KS <rodrigoks@gmail.com>
2025-04-01 16:10:47 -0300

    Códigos iniciais.

---
+++ src/test/java/br/com/jbusiness/cardapio/CardapioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jbusiness.cardapio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
